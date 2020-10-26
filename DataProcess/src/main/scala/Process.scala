ce7565dc36904f42fad5f2cea946e655396933b2
Vlado <vlado>
2020-10-26 15:22:27 +0100

    Save result to mongo

--- DataProcess/src/main/scala/Process.scala
+++ DataProcess/src/main/scala/Process.scala
@@ -1,23 +1,12 @@
-import java.io.Serializable
-
-import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.{MongoSpark, toSparkContextFunctions}
 import com.mongodb.spark.config._
-import com.mongodb.spark.sql._
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.rdd.RDD
 import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 import org.bson.Document
-import org.json4s.DoubleJsonFormats.GenericFormat
-import org.json4s.jsonwritable
-import org.mongodb
-import org.mongodb.scala._
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 
 
 object Process  extends App {
-    SparkSession.clearActiveSession()
+  SparkSession.clearActiveSession()
   val spark = SparkSession.builder()
     .master("local")
     .appName("covid")
@@ -29,41 +18,15 @@ object Process  extends App {
   val rdd = MongoSpark.load(spark.sparkContext, readConfig)
   val writeConfig = WriteConfig(Map("collection" -> "covid_report", "writeConcern.w" -> "majority"), Some(WriteConfig(spark.sparkContext)))
   val dates = rdd.map(doc => (doc.getInteger("date")))
+  val distinctDate = dates.distinct().collect().toList
 
-  val distinct = dates.distinct()
-
-  println(distinct.count())
-
-
-
-  distinct.foreach(date => {
-
-    val x = date
-
-    println(x)
+  distinctDate.foreach(x => {
     val covid = Data.getCovid(x, 14)
     val events = Data.getEvents(x)
-    val joined = covid.leftOuterJoin(events)
-    joined.foreach(x => println(x))
-    println(covid.count())
-    println(events.count())
-    //val documents = joined.map(result => Document.parse(s"{state : ${result._1.toString}, cases: ${result._2._1}, rsvp: ${result._2._2}"))
-
-    //documents.foreach(x => println(x))
-    //println(documents.count())
-    //MongoSpark.save(documents, writeConfig)
-
-    println("Finish")
+    val joined = covid.leftOuterJoin(events).cache()
+    val documents = joined.map(result => Document.parse(s"{date: $x, state : ${'"'}${result._1}${'"'}, cases: ${result._2._1}, rsvp: ${'"'}${result._2._2}${'"'}}"))
+    MongoSpark.save(documents, writeConfig)
   })
-
-//  val date = 20200307
-//  val covid = Data.getCovid(date, 14)
-//  val events = Data.getEvents(date)
-//
-//  val joined = covid.leftOuterJoin(events)
-//  events.foreach(x => println(x))
-//  joined.foreach(x => println(x))
-
 }
 
 
